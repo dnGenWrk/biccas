ce089d3f194c9f74f6062d3dfdaaf93acc2c84ab
Dnworkf <general-n-dnmf@outlook.com>
2023-08-02 23:49:14 +0300

    upd

--- src/components/choose-plan/switch_prices.js
+++ src/components/choose-plan/switch_prices.js
@@ -3,17 +3,22 @@
 (function () {
   const buttons = document.querySelectorAll(".cta-prices");
   const buttonsWrapper = document.getElementById("cta-buttons-wrapper");
+  const businessPlanPrices = document.querySelector(".businessPlan");
+
   const ctaClick = function () {
     buttonsWrapper.dataset.activeBill = this.dataset.cta;
+    if (buttonsWrapper.dataset.activeBill === "yearly") {
+      businessPlanPrices.innerHTML = 280;
+      businessPlanPrices.classList.add("price_amount_animate");
+    } else {
+      businessPlanPrices.innerHTML = 16;
+    }
     buttons.forEach((e) => {
       e.classList.remove("active");
     });
     this.classList.add("active");
-
-    console.log(this.dataset.cta);
   };
   buttons.forEach((e) => {
     e.addEventListener("click", ctaClick);
   });
-  console.log("!!! Switch prices test");
 })();
